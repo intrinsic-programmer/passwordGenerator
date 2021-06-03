9a388227540498061aab11ba29e360f276a8e573
Joy <gajjarjoy4@gmail.com>
2021-06-03 09:52:39 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+
+letters=string.ascii_letters
+digits=string.digits
+special_char=string.punctuation
+
+possibilities=[]
+possibilities.extend(list(letters))
+possibilities.extend(list(digits))
+possibilities.extend(list(special_char))
+random.shuffle(possibilities)
+password_length=int(input("Enter Your Password length"))
+print("".join(possibilities[0:password_length]))
